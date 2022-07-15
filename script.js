c829f5614088b02c7e08b3ec992f152183fd23c5
Jorge Matinez <jorjonicodcv@gmail.com>
2022-07-15 12:30:50 -0300

    mas cambios en link

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ class Producto {
     }
 }
 //productos + array de productos
-const producto1 = new Producto(1, "Toro", "Hombre", "Adultos", "negro", 950, 50, `/img/negro.webp`)
-const producto2 = new Producto(2, "Pantera", "Mujer", "Adultos", "negro", 900, 50, `/img/negro.webp`)
-const producto3 = new Producto(3, "Leona", "Mujer", "Adultos", "rojo", 900, 50, `/img/rojo.webp`)
-const producto4 = new Producto(4, "Agila", "Hombre", "Adultos", "rojo", 950, 50, `/img/rojo.webp`)
-const producto5 = new Producto(5, "Tigre", "Hombre", "Niños", "azul", 750, 50, `/img/azul.webp`)
-const producto6 = new Producto(6, "Chita", "Mujer", "Niños", "rosa", 700, 50, `/img/rosa.webp`)
-const producto7 = new Producto(7, "Pantera", "Unisex", "Niños", "verde", 600, 50, `/img/verde.webp`)
-const producto8 = new Producto(8, "Iguana", "Unisex", "Niños", "verde", 750, 50, `/img/verde.webp`)
-const producto9 = new Producto(9, "Ave", "Unisex", "Niños", "azul", 500, 50, `/img/azul.webp`)
-const producto10 = new Producto(10, "Jaguar", "Unisex", "Niños", "negro", 500, 50, `/img/negro.webp`)
+const producto1 = new Producto(1, "Toro", "Hombre", "Adultos", "negro", 950, 50, `img/negro.webp`)
+const producto2 = new Producto(2, "Pantera", "Mujer", "Adultos", "negro", 900, 50, `img/negro.webp`)
+const producto3 = new Producto(3, "Leona", "Mujer", "Adultos", "rojo", 900, 50, `img/rojo.webp`)
+const producto4 = new Producto(4, "Agila", "Hombre", "Adultos", "rojo", 950, 50, `img/rojo.webp`)
+const producto5 = new Producto(5, "Tigre", "Hombre", "Niños", "azul", 750, 50, `img/azul.webp`)
+const producto6 = new Producto(6, "Chita", "Mujer", "Niños", "rosa", 700, 50, `img/rosa.webp`)
+const producto7 = new Producto(7, "Pantera", "Unisex", "Niños", "verde", 600, 50, `img/verde.webp`)
+const producto8 = new Producto(8, "Iguana", "Unisex", "Niños", "verde", 750, 50, `img/verde.webp`)
+const producto9 = new Producto(9, "Ave", "Unisex", "Niños", "azul", 500, 50, `img/azul.webp`)
+const producto10 = new Producto(10, "Jaguar", "Unisex", "Niños", "negro", 500, 50, `img/negro.webp`)
 
 const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto8, producto9,]
 //Accedo a un elemento HTML mediante su ID
